e9d2803e02a09d404063c55857ccfbd0012e9253
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2021-03-25 10:39:58 +0700

    Add Cors policy and Fetch data with Axios

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -36,8 +36,19 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
-            services.AddDbContext<DataContext>(opt => {
+            services.AddDbContext<DataContext>(opt =>
+            {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .WithOrigins("http://localhost:3000");
+                });
             });
         }
 
@@ -54,6 +65,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 

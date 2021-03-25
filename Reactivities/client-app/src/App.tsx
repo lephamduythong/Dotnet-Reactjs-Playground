77332a2581aaedcd9c653af004ba98ddaea5627f
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2021-03-25 11:08:16 +0700

    Add Sematic UI React

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+import { List, Header } from "semantic-ui-react";
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -13,16 +14,14 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {activities.map((activity: any) => (
-            <li key={activity.id}>{activity.title}</li>
-          ))}
-        </ul>
-      </header>
-    </div>
+    <>
+      <Header as="h2" icon="users" content="Reactivities" />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </>
   );
 }
 
